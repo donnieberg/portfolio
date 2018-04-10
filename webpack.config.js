6823afffea5f8387df8537115ade8d6d97d545db
Donielle Berg <donielle.berg@gmail.com>
2018-04-09 19:43:12 -0700

    Update npm modules and update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 var path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -27,13 +26,13 @@ module.exports = {
         exclude: /node_modules/,
         loaders: 'babel-loader',
         options: {
-          presets: ['es2015', 'react']
+          presets: ['env', 'react']
         }
       },
       {
         test: /\.jsx$/,
         loaders: [
-          'babel-loader?cacheDirectory&presets[]=react&presets[]=es2015', // invokes Babel to translate React and ES6
+          'babel-loader?cacheDirectory&presets[]=react&presets[]=env', // invokes Babel to translate React and ES6
         ]
       },
 
@@ -48,24 +47,17 @@ module.exports = {
           'sass-loader?sourceMap'
         ],
         exclude: /node_modules|lib/
-
-        /*
-        test: /\.scss$/,
-        //Moves code into css file to link from index.html instead of putting it into a style tag (can't cache that shit)
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
-        */
-      },
+      }
     ]
   },
 
   plugins: [
+		/*
     new ExtractTextPlugin({
       filename: 'dist/style.css',
       allChunks: true
     })
+		*/
   ]
 
 };
