c260917749cf0b79fb396635efabb0dbdb143063
Doneille Berg <d.berg@dberg-ltm.internal.salesforce.com>
2023-09-02 21:05:21 -0700

    Start portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
+import '../node_modules/colors.css/css/colors.min.css';
 import './App.css';
+import data from './data.js';
+import Tabset from './tabset.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="df ptxx phxx">
+            <header className="header">
+                <h1 className="phs dib bg-blue text-1 white bold tracking-tight">Donielle Berg</h1>
+                <p className="mts text-3 bold tracking-tight">Lead Accessibility Engineer</p>
+                <p className="mts text-3">I build accessible, inclusive, and useful products on the web.</p>
+                <nav className="nav text-4 caps bold">
+                    <ul className="list-plain">
+                        <li className="mvm"><a href="#">About</a></li>
+                        <li className="mvm"><a href="#">Projects</a></li>
+                        <li className="mvm"><a href="#">Experience</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <main className="main pll text-3">
+                <div>
+                    {data.about}
+                </div>
+                <div className="mvx">
+                    <div className="df df-end">
+                        <hr className="df-grow mrm mv-auto" />
+                        <h2 className="text-2 caps bold">Projects</h2>
+                    </div>
+                    <article>
+                        <h3 className="text-3">Figma A11y Annotations Library</h3>
+                        <p className="mtl">{data.figmaA11y}</p>
+                        <Tabset />
+                    </article>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,6 @@
+const labels = {
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    figmaA11y: "A11y Annotations in designs help engineers build accessibly from the beginning rather than having to fix a bunch of bugs at the end. I created a Figma A11y Annotation Library to help designers do this more efficiently and create cohesion in the language, design, and best practices for engineers."
+};
+
+export default labels;
