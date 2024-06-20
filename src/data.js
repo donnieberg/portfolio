4f31b081ddc6e367665b9f60ae9eea45e614d593
Donielle Berg <donielle.berg@servicenow.com>
2024-06-19 19:29:59 -0700

    update portfolio

--- src/data.js
+++ src/data.js
@@ -1,25 +1,38 @@
 const labels = {
-    figmaA11y: "A11y Annotations in designs help engineers build accessibly from the beginning rather than having to fix a bunch of bugs at the end. I created a Figma A11y Annotation Library to help designers do this more efficiently and create cohesion in the language, design, and best practices for engineers.",
-    dreamforceProject: "I co-created an interactive game for customers to experience inaccessibility first hand. We had two computers: one with an accessible conference website, the other with an inaccessible version. The challenge was who could register the fastest for the conference. There were so many ‘ah-ha!’ moments when the inaccessible version prevented people from completing. Customers told us this was the most impactful experience at Dreamforce.",
-    prototype: "In updating our Design System, I co-created a react prototype to test out different versions of our components with People with Disabilities. I also wrote the script for user research (executed by Level Access). The project provided clear direction for our Design System team to update the components to WCAG 2.2 and improve the overall user experience.",
-    experience1: {
-        year: "2017-Present",
-        title: "Lead Accessibility Engineer",
-        company: "Salesforce",
-        description: "I lead the Accessibility for our Design Systems UX and Engineering teams. I previously led the Accessibility Program for B2B and B2C Commerce Products at Salesforce. My role spans a wide spectrum of partnering with executive leadership in goal planning/execution, coaching and enablement for engineers and designers, reviewing designs/code, and writing specs and documentation. I also build prototypes in HTML, CSS, Javascript, and React for user research/design exploration and create tools for designers and engineers to build a11y more seamlessly."
-    },
-    experience2: {
-        year: "2015-2017",
-        title: "Senior Front End Developer",
-        company: "Salesforce",
-        description: "As part of the IoT and Analytics Clouds, I built responsive and accessible UIs using HTML, CSS, Javascript, and React. I’m the #2 of 100 contributors for the Salesforce React open source Design System, and our library is part of Open Base’s 10 Best React Design Systems."
-    },
-    experience3: {
-        year: "2009-2013",
-        title: "Event Planner",
-        company: "KIPP Foundation",
-        description: "I managed the operations of professional development events for 5,000+ teachers and principals across the country."
-    },
+  figmaA11y:
+    "A11y Annotations help engineers build accessible UIs from the beginning rather than fixing a bunch of bugs at the end. I created a Figma A11y Annotation Library to help designers do this more efficiently and create cohesion in the language, design, and best practices for engineers.",
+  dreamforceProject:
+    "I co-created an interactive game for customers to experience inaccessibility first hand. We had two computers: one with an accessible conference website, the other with an inaccessible version. The challenge was who could register the fastest for the conference. There were so many ‘ah-ha!’ moments when the inaccessible version prevented people from completing. Customers told us this was the most impactful experience at Dreamforce.",
+  prototype:
+    "In updating our Design System, I co-created a react prototype to test out different versions of our components with People with Disabilities. I also wrote the script for user research (executed by Level Access). The project provided clear direction for our Design System team to update the components to WCAG 2.2 and improve the overall user experience.",
+  experience1: {
+    year: "2023-Present",
+    title: "Senior Accessibility Engineer",
+    company: "ServiceNow",
+    description:
+      "Leads Accessibility for the Condition Builder product team and contributes to overall front end UI. Writes a11y specs for features, leads a11y training, conducts a11y reviews, and serves as our team’s Design Lead to collaborate with UX and PMs.",
+  },
+  experience2: {
+    year: "2017-2023",
+    title: "Lead Accessibility Specialist",
+    company: "Salesforce",
+    description:
+      "I  led Accessibility for the Design Systems UX and Engineering teams where I developed a11y specs for new components, reviewed design mocks, and worked with engineers on implementation. I also built prototypes in HTML, CSS, Javascript, and React for user research & design exploration and created tools for designers to build a11y more seamlessly.",
+  },
+  experience3: {
+    year: "2015-2017",
+    title: "Senior UX Developer",
+    company: "Salesforce",
+    description:
+      "As part of the UX team, I built responsive and accessible components using HTML, CSS, Javascript, and React. I’m the #2 of 100 contributors for the Salesforce React open source Design System, and our library is part of Open Base’s 10 Best React Design Systems.",
+  },
+  experience4: {
+    year: "2010-2013",
+    title: "Event Planner",
+    company: "KIPP Foundation",
+    description:
+      "I managed the operations of professional development events for 5,000+ teachers and principals across the country.",
+  },
 };
 
 export default labels;
